af55eb84bf6c42782edb40d68b971a284336849a
Rishabh <coolrishabh122@gmail.com>
2025-05-25 23:20:20 +0530

    push

---
+++ script.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+
+const dropdowns = document.querySelector(".dropdown select");
+
+for (code in countryList ) {
+    console.log(code);
+
+}
